bdad2d1073fae908b3737d018d39290620e0525a
Leonid Varsaminov <lvarsaminov@gmail.com>
2026-06-12 21:26:02 +0300

    Identity + Roles setup

---
+++ CollegeNBU/CollegeNBU.Data/Data/SeedData.cs
@@ -0,0 +1,59 @@
+﻿using CollegeNBU.Core.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CollegeNBU.Data.Data;
+
+public static class SeedData
+{
+    public static async Task SeedRolesAsync(IServiceProvider serviceProvider)
+    {
+        var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+        string[] roles = new[]
+        {
+            "Admin",
+            "Rector",
+            "DepartmentHead",
+            "Teacher",
+            "Student"
+        };
+
+        foreach (var role in roles)
+        {
+            if (!await roleManager.RoleExistsAsync(role))
+            {
+                await roleManager.CreateAsync(new IdentityRole(role));
+            }
+        }
+    }
+
+    public static async Task SeedAdminAsync(IServiceProvider serviceProvider)
+    {
+        var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+        string adminEmail = "admin@college.com";
+        string adminPassword = "Admin123!";
+
+        var admin = await userManager.FindByEmailAsync(adminEmail);
+
+        if (admin == null)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = adminEmail,
+                Email = adminEmail,
+                FirstName = "System",
+                LastName = "Admin",
+                EmailConfirmed = true
+            };
+
+            var result = await userManager.CreateAsync(user, adminPassword);
+
+            if (result.Succeeded)
+            {
+                await userManager.AddToRoleAsync(user, "Admin");
+            }
+        }
+    }
+}
